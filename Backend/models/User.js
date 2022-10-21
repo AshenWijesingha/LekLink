f5df7e9172234f14eda99925a228ff70486c9f4e
Vibodha Gallage <98052908+Vibo22@users.noreply.github.com>
2022-10-21 06:51:32 -0700

    Backend Commit

---
+++ Backend/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    
+    name : {
+        type : String,
+        required: true
+
+    },
+
+    email : {
+        type: String,
+    },
+    password : {
+        type: String,
+    },
+
+    type : {
+        type: String,
+    }
+})
+
+const User = mongoose.model("User",UserSchema);
+
+module.exports = User;
