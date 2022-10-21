d014d056a95cdf03cff014f7e975a0a3718c948d
IT20652814 <it20652814@my.sliit.lk>
2022-10-21 18:49:00 +0530

    new frontend commit

--- frontend/src/components/AddRecording.js
+++ frontend/src/components/AddRecording.js
@@ -134,7 +134,7 @@ export default function AddRecording() {
 
               <div className="col-3">
                   <button className="btn btn-primary" type="submit" >
-                  <a href="/allRECORDING-table" validate="true"style={{textDecoration:'none', color:'black'}}></a>
+                  <a href="/allRECORDING-table" validate="true"style={{textDecoration:'none', color:'white'}}></a>
                     Submit</button>
               </div>
       </form>
